d7ecb9bec829cb692dfa62658287d1d3ed66e1cb
Annie Jung <younjujung27@gmail.com>
2022-04-12 08:39:57 -0700

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+
+  // state variable to store poekemon data from API
+  const [pokemons, setPokemons] = useState([])
+
+  // useEffect - to do API calls only when the component first renders. 
+  useEffect(()=> {
+    axiosPokemons();
+  },[])
+
+  // use Axios 
+  const axiosPokemons = () => {
+    // console.log('axios button is working too!')
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then((res) => {
+        console.log(res.data.results)
+        setPokemons(res.data.results)
+      })
+      .catch(err => console.log(err))
+  }
+
+    
+// use Vanila JS 
+  // const fetchPokemons = ()=> {
+  //   console.log('fetch button is working!')
+  //   fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+    
+  //     .then(res => {
+  //       return res.json()
+  //     })
+  //     .then(jsonRes => {
+  //       setPokemons(jsonRes.results)
+  //       console.log(jsonRes.results)
+  //     })
+  //     .catch(someErr => console.log(someErr))
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon API</h1>
+      <button onClick={axiosPokemons}>Fetch Pokemons</button>
+      <hr/>
+      {/* {JSON.stringify(pokemons)} */}
+      
+      <ul>
+        {
+              pokemons.map((pokemon, i)=>{
+                return (
+                  <li key={i}>{pokemon.name}</li> 
+                  
+                              
+                )
+              })
+            }
+      </ul>
+        
+      
+        
+      
+
     </div>
   );
 }
